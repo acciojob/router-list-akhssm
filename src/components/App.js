c8dcf55ca4d3957303463d71741d8d3fc46c2533
Akshay <mandaakshay20@gmail.com>
2025-12-10 12:48:54 +0530

    Wrote code according to the given description

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
-
 import React from "react";
-import './../styles/App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ItemList from "./ItemList";
+import ItemDetail from "./ItemDetail";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <Router>
+      <Switch>
+        <Route exact path="/" component={ItemList} />
+        <Route path="/items/:id" component={ItemDetail} />
+      </Switch>
+    </Router>
+  );
+};
 
-export default App
+export default App;
